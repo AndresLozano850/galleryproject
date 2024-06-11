c4abdf3bc6ab0caef51302f8b74c942fd1b12969
Andrés Lozano <camilolozano850@gmail.com>
2024-06-10 22:57:33 -0500

    Gallery Dogs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,63 +12,63 @@ import Labrador from './Labrador'
 import Schnauzer from './Schnauzer'
 const Navigation = () => {
   return (
-    <div>
-        <Link to="/bichon_maltes">
-          <figure>
+    <div className="container mt-4 father">
+        <Link to="/bichon_maltes" className="links">
+          <figure className="image-size">
             <Bichon_maltes />
             <figcaption>Bichon Maltes</figcaption>
           </figure>
         </Link>
-        <Link to="/bulldog">
-          <figure>
+        <Link to="/bulldog" className="links">
+          <figure className="image-size">
             <Bulldog />
             <figcaption>Bulldog</figcaption>
           </figure>
         </Link>
-        <Link to="/chihuahua">
-          <figure>
+        <Link to="/chihuahua" className="links">
+          <figure className="image-size">
             <Chihuahua />
             <figcaption>Chihuahua</figcaption>
           </figure>
         </Link>
-        <Link to="/chow_chow">
-          <figure>
+        <Link to="/chow_chow" className="links">
+          <figure className="image-size">
             <Chow_chow />
             <figcaption>Chow Chow</figcaption>
           </figure>
         </Link>
-        <Link to="/cocker_spaniel">
-          <figure>
+        <Link to="/cocker_spaniel" className="links">
+          <figure className="image-size">
             <Cocker_spaniel />
             <figcaption>Cocker Spaniel</figcaption>
           </figure>
         </Link>
-        <Link to="/doberman">
-          <figure>
+        <Link to="/doberman" className="links">
+          <figure className="image-size">
             <Doberman />
             <figcaption>Doberman</figcaption>
           </figure>
         </Link>
-        <Link to="/golden_retriever">
-          <figure>
+        <Link to="/golden_retriever" className="links">
+          <figure className="image-size">
             <Golden_retriever />
             <figcaption>Golden Retriever</figcaption>
           </figure>
         </Link>
-        <Link to="/husky_siberiano">
-          <figure>
+        <Link to="/husky_siberiano" className="links">
+          <figure className="image-size">
             <Husky_siberiano />
             <figcaption>Husky Siberiano</figcaption>
           </figure>
         </Link>
-        <Link to="/labrador">
-          <figure>
+        <Link to="/labrador" className="links">
+          <figure className="image-size">
             <Labrador />
             <figcaption>Labrador</figcaption>
           </figure>
         </Link>
-        <Link to="/schnauzer">
-          <figure>
+        <Link to="/schnauzer" className="links">
+          <figure className="image-size">
             <Schnauzer />
             <figcaption>Schnauzer</figcaption>
           </figure>
